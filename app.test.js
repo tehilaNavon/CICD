a5b0d0a5c6247904b377d6b16d4b528f2987c5be
Tehila Navon <t0548408109@gmail.com>
2026-04-28 15:59:02 +0300

    check test

--- app.test.js
+++ app.test.js
@@ -3,5 +3,5 @@ const test = require('node:test');
 const { add } = require('./app');
 
 test('Check if add(2, 3) equals 5', () => {
-    assert.strictEqual(add(2, 3), 6);
+    assert.strictEqual(add(2, 3), 5);
 });
